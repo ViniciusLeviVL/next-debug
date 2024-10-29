f13893a2726fe012649193bf2b4a36c0e6e8607e
ViniciusLeviVL <87581689+ViniciusLeviVL@users.noreply.github.com>
2024-10-29 11:37:45 -0300

    add bunnycdn to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.NEXT_PUBLIC_CDN_BASE_URL.split("/").pop()
+      }
+    ],
+    deviceSizes: [640, 750, 1080, 1280]
+  },
+};
 
 export default nextConfig;
